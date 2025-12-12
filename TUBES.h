cf0e04a5e8ff5b226ea3f3596d20a5d71b81a94d
shakira <147226750+strongerwo@users.noreply.github.com>
2025-12-12 16:05:08 +0700

    Update TUBES.h

--- TUBES.h
+++ TUBES.h
@@ -36,6 +36,21 @@ struct listKursus{
     adrKursus last;
 };
 
+void createList(listKursus &L);
+adrKursus createElemenKursus(kursus k);
+adrModul createElemenModul(modul m);
+void insertLastKursus(listKursus &L, adrKursus P);
+void insertLastModul(adrKursus K, adrModul M);
+adrKursus findKursus(listKursus L, string kode);
+void deleteKursus(listKursus &L, string kode);
+void tampilModul(adrKursus K);
+void tampilKursus(listKursus L);
+void cariBerdasarkanLevel(listKursus L, string level);
+void cariBerdasarkanJudul(listKursus L, string judul);
+void cariDurasiKurang(listKursus L, int maxDurasi);
+int hitungPerLevel(listKursus L, string level);
+adrKursus kursusModulTerbanyak(listKursus L);
+
 
 
 
