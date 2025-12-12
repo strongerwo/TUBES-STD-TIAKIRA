4d0dcf2198d631c2c2e94321819266d8762eb959
tianisasianipar <tianisasianipar@gmail.com>
2025-12-12 16:31:32 +0700

    Update TUBES.h

--- TUBES.h
+++ TUBES.h
@@ -7,51 +7,69 @@ using namespace std;
 typedef struct elemenKursus *adrKursus;
 typedef struct elemenModul *adrModul;
 
-struct kursus{
+struct kursus {
     string kode;
     string judul;
     string level;
     int durasi;
 };
 
-struct modul{
+struct modul {
     string nama;
 };
 
-struct elemenKursus{
+struct elemenKursus {
     kursus info;
     adrKursus next;
     adrKursus prev;
     adrModul firstModul;
 };
 
-struct elemenModul{
+struct elemenModul {
     modul info;
     adrModul next;
     adrModul prev;
 };
 
-struct listKursus{
+struct listKursus {
     adrKursus first;
     adrKursus last;
 };
 
-void createList(listKursus &L);
-adrKursus createElemenKursus(kursus k);
-adrModul createElemenModul(modul m);
+
+void createListKursus(listKursus &L);
+adrKursus alokasiKursus(kursus x);
+void dealokasiKursus(adrKursus &P);
+void insertFirstKursus(listKursus &L, adrKursus P);
 void insertLastKursus(listKursus &L, adrKursus P);
-void insertLastModul(adrKursus K, adrModul M);
+void insertAfterKursus(listKursus &L, adrKursus Prec, adrKursus P);
+void deleteFirstKursus(listKursus &L, adrKursus &P);
+void deleteLastKursus(listKursus &L, adrKursus &P);
+void deleteAfterKursus(listKursus &L, adrKursus Prec, adrKursus &P);
 adrKursus findKursus(listKursus L, string kode);
-void deleteKursus(listKursus &L, string kode);
-void tampilModul(adrKursus K);
-void tampilKursus(listKursus L);
-void cariBerdasarkanLevel(listKursus L, string level);
-void cariBerdasarkanJudul(listKursus L, string judul);
-void cariDurasiKurang(listKursus L, int maxDurasi);
-int hitungPerLevel(listKursus L, string level);
-adrKursus kursusModulTerbanyak(listKursus L);
-
-
-
+void showAllKursus(listKursus L);
+int countKursus(listKursus L);
+adrModul alokasiModul(modul x);
+void dealokasiModul(adrModul &P);
+void insertFirstModul(adrKursus P, adrModul M);
+void insertLastModul(adrKursus P, adrModul M);
+void insertAfterModul(adrKursus P, adrModul Prec, adrModul M);
+void deleteFirstModul(adrKursus P, adrModul &M);
+void deleteLastModul(adrKursus P, adrModul &M);
+void deleteAfterModul(adrKursus P, adrModul Prec, adrModul &M);
+adrModul findModul(adrKursus P, string nama);
+void showAllModul(adrKursus P);
+int countModul(adrKursus P);
+void connectModulToKursus(adrKursus K, modul dataModul);
+void disconnectModulFromKursus(adrKursus K, string namaModul);
+void showKursusWithModul(listKursus L);
+adrKursus findKursusMaxModul(listKursus L);
+adrKursus findKursusMinModul(listKursus L);
+int countKursusByLevel(listKursus L, string level);
+int countKursusByDuration(listKursus L, int minDur, int maxDur);
+void searchKursusByLevel(listKursus L, string level);
+void deleteAllModulFromKursus(adrKursus K);
+void deleteKursusWithModul(listKursus &L, string kode);
+float averageDurasiKursus(listKursus L);
 
 #endif // TUBES_H_INCLUDED
